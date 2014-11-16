d3d947b22b0f480f5806296fd70d6bdb56616765
MarshalJoe <joseph.charles.marshall@gmail.com>
2014-11-16 09:43:07 -0600

    Testing

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,8 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+		
+		// This provides a test
+		$scope.test = 'test success!';
 	}
 ]);
